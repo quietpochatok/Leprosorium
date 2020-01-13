78b6b7720635845176224a2455ee096dcbde83ef
quietpochatok <bosroin@gmail.com>
2020-01-13 20:14:39 +0500

    init db & def init DB & before do

--- app.rb
+++ app.rb
@@ -2,6 +2,18 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+@db= SQLite3::Database.new 'leprosorium.db'
+@db.results_as_hash = true #резyльтат в виде хэша
+
+end
+
+
+before do
+init_db
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
