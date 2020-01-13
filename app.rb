d2766bc4ef9efce165e6f7e55cca866d29feb5bc
quietpochatok <bosroin@gmail.com>
2020-01-13 19:04:33 +0500

    add stub for newPosts

--- app.rb
+++ app.rb
@@ -6,3 +6,6 @@ require 'sinatra/reloader'
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+get '/new_post' do
+erb 'hi'
+end
