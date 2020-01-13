ee84b02d65266ba42a0c5826c59f6b0bd5437317
quietpochatok <bosroin@gmail.com>
2020-01-13 19:21:52 +0500

    add  new erb

--- app.rb
+++ app.rb
@@ -7,5 +7,5 @@ get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 get '/new_post' do
-erb 'hi'
+erb :new_post
 end
