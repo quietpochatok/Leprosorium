db6ec01f611ee5cfe8d1a8c4d72f2d5cc3471d2f
quietpochatok <bosroin@gmail.com>
2020-01-13 20:29:45 +0500

    init folder for db

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require 'sinatra/reloader'
 require 'sqlite3'
 
 def init_db
-@db= SQLite3::Database.new 'leprosorium.db'
+@db= SQLite3::Database.new 'db/leprosorium.db'
 @db.results_as_hash = true #резyльтат в виде хэша
 
 end
